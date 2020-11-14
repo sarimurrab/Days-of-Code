97f15cf3353afa5803a53b3cdeb6575004dd6e9c
Chaudhary Sarimurrab <samurrab@gmail.com>
2020-11-15 00:46:04 +0530

    commit by SARIMs BOT on 15-11-2020

--- Winter_20/Day5/4min_subset_differnce.cpp
+++ Winter_20/Day5/4min_subset_differnce.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int min_subset_diff(int * arr, int n)
@@ -9,13 +9,62 @@ int min_subset_diff(int * arr, int n)
         range+=arr[i];
 
     
-    //to get the last row of subset sum
-    int t[n+1][range+1];
+    // To get the last row of subset sum...
+    // which include the all possible sum using differnt subsets....
+    bool t[n+1][range+1];
     //step1 - initialization
+    for(int i=0;i<n+1;i++)
+    for(int j=0;j<range+1;j++)
+    {
+        if(i==0)
+        t[i][j] = false;
+        if(j==0)
+        t[i][j] = true;
+    }
+
+    //step 2 - top down process
+    for(int i=1;i<n+1;i++)
+    for(int j=1;j<range+1;j++)
+    {
+        if(arr[i-1]<=j)
+        {
+            t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+        }
+        else
+        {
+            t[i][j] = t[i-1][j];
+        }
+    }
+
+    // This is the half range we were looking for
+    vector<int> v;
+    for(int j=0;j<=range/2;j++)
+    {
+        if(t[n][j])
+        v.push_back(j);
+    }
+
+
+    // FORMULA WE CALCULATED = Range - 2 s1
+    int mini = INT_MAX;
+    for(int i=0;i<v.size();i++)
+    {
+        mini = min(mini, range-2*v[i]);
+    }
+
+
+    return mini;
 }
 
 
 int main()
 {
+    int arr[] = {1,2,7};
+    int n = sizeof(arr)/sizeof(int);
+    cout<<min_subset_diff(arr,n);
+
+
+
+    return 0;
 
 }
